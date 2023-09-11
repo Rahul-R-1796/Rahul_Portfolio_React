ec8b79ee4d8b9d585f89998f2d35290d6cc2a6f4
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-12 00:37:01 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,8 +10,8 @@ function Projects() {
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
-        <p>Here are a few projects I've worked on recently.
-        </p>
+        <h3>Here are a few projects I've worked on recently.
+        </h3>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           {PROJECTS.map((project, index) => (
             <Col md={4} className="project-card" key={index}>
