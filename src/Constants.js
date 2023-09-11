60c3706fc14829fe42240a094b0b2f20a0d5529d
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-12 00:57:52 +0530

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,29 +1,16 @@
-import AUTOMATION from "./Assets/Projects/3automation.jpg";
-import COAST from "./Assets/Projects/coast.jpg";
-import DROPSHIP from "./Assets/Projects/dropship.jpg";
-import GOOD_FOOD_DISCOUNTS from "./Assets/Projects/goodFoodDiscounts.png";
-import GUAMEDIA from "./Assets/Projects/guamedia.jpg";
-import HSP from "./Assets/Projects/hsp.png";
-import COSMOS from "./Assets/Projects/cosmos.jpg";
-import TECHSPEC from "./Assets/Projects/techspec.png";
-import TRAVELWELL from "./Assets/Projects/travelwell.png";
+import Bikebooking from "./Assets/Projects/Bikebooking.png";
 
 
 export const PROJECTS = [
   {
-    image: COSMOS,
-    name: "Micorsoft Azure Cosmos DB",
-    technologyUsed: "ReactJs, Javascript, Redux, knockout.js, Jest",
-    description: "Azure Cosmos DB is Microsoft's proprietary globally distributed, multi-model database service 'for managing data at planet-scale' Azure Cosmos DB is a fully managed NoSQL database for modern app development. Single-digit millisecond response times, and automatic and instant scalability, guarantee speed at any scale.",
+    image: Bikebooking,
+    name: "Bike Service Booking System",
+    technologyUsed: "MERN (MongoDB, Express.js, React, Node.js), JavaScript",
+    description: "A user-friendly MERN stack application for hassle-free bike service bookings. Features real-time updates, secure authentication, and payment integration. Streamlining bike maintenance for users.",
     url: "https://cosmos.azure.com/",
+    githubLink:"https://github.com/Rahul-R-1796/Bike_Service_App/"
   },
-  {
-    image: TRAVELWELL,
-    name: "travlwell.com",
-    technologyUsed: "Shopify, React, typescript, polaris, GraphQL",
-    description: "Custom Luggage Tags and Passport Holders engraved with everywhere you've traveled and everywhere you will travel in the future.",
-    url: "https://travlwell.com/",
-  }
+
 ];
 
 export const SKILLS = [
