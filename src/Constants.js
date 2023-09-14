43376055f7623e2ba1e45a701de11e0a008cfb98
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-14 11:25:40 +0530

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -6,9 +6,7 @@ export const PROJECTS = [
     image: Bikebooking,
     name: "Bike Service Booking System",
     technologyUsed: "MERN (MongoDB, Express.js, React, Node.js), JavaScript",
-    description: "Designed and developed a MERN-based Bike Service Booking App, offering users the convenience of reserving service slots in advance. 
-      The app features three daily booking slots, while an exclusive admin panel enables administrators to access and manage booking details, 
-  including the ability to cancel reservations, ensuring efficient service operations..",
+    description: "Designed and developed a MERN-based Bike Service Booking App, offering users the convenience of reserving service slots in advance. The app features three daily booking slots, while an exclusive admin panel enables administrators to access and manage booking details, including the ability to cancel reservations, ensuring efficient service operations..",
     url: "https://64bff69778b4c600867df6dd--snazzy-alpaca-58be59.netlify.app/",
     githubLink:"https://github.com/Rahul-R-1796/Bike_Service_App/"
   },
@@ -17,9 +15,7 @@ export const PROJECTS = [
     image: Stackoverflow,
     name: "Stack Overflow Clone Website",
     technologyUsed: "MERN (MongoDB, Express.js, React, Node.js)",
-    description: "This MERN Stack Overflow clone, featuring user authentication, Q&A functionality, 
-      robust search capabilities, and a voting system. Users can add friends, and manage friend requests. 
-      The platform fosters a vibrant programming community, with a dedicated notifications page for friend requests and question interactions.",
+    description: "This MERN Stack Overflow clone, featuring user authentication, Q&A functionality, robust search capabilities, and a voting system. Users can add friends, and manage friend requests. The platform fosters a vibrant programming community, with a dedicated notifications page for friend requests and question interactions.",
     url: "https://stackverflow-clone.vercel.app/",
     githubLink:"https://github.com/Rahul-R-1796/Stackverflow_Clone"
   },
