e48fef62d5099fc4106f8d1f525f515d1ea116fd
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-15 18:43:35 +0530

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -22,13 +22,16 @@ export const PROJECTS = [
   },
       {
     image: Razorpay,
-    name: "Stack Overflow Clone Website",
+    name: "Razorpay E-Commerce Website",
     technologyUsed: "MERN (MongoDB, Express.js, React, Node.js)",
     description: "The MERN Razorpay app is a full-stack web application built using the MERN (MongoDB, Express.js, React, Node.js) stack and integrated with Razorpay for seamless online payment processing.",
     url: "https://razorpay-mern.vercel.app/",
     githubLink:"https://github.com/Rahul-R-1796/Razorpay-MERN.git"
   },
 ];
+
+
+
 
 export const SKILLS = [
   { name: "ReactJs", initialRating: 3 },
