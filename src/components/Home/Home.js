96eef735299b502b314ee398942102c9369c6323
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-12-20 17:52:25 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/profile.png";
+import myImg from "../../Assets/Profile_Pic.JPEG";
 import SocialMedia from "../SocialMedia";
 import TypeWriter from "./TypeWriter";
 
